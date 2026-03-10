89f5686fff2b4aa599c7fd5ea53d60cdc1d7fcc7
alejandro bernal <alejogevara27@gmail.com>
2026-03-10 09:59:55 -0600

    nuevo

--- portafolio/index.php
+++ portafolio/index.php
@@ -1,33 +1,88 @@
-<<div class="secciones">
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<title>Portafolio QA Tester</title>
+<link rel="stylesheet" href="estilos.css">
+</head>
 
-    <div class="proyectos">
-        <h2>Proyectos de Testing:</h2>
+<body>
 
-        <p>
-        <img src="img/github.png" width="35">
-        Periodico Digital Comunitario
-        </p>
+<div class="contenedor">
 
-        <p>
-        <img src="img/github.png" width="35">
-        Donamiga
-        </p>
+    <div class="perfil">
+        <img src="img/_LUI0053-2.jpeg" alt="foto">
     </div>
 
-    <div class="linea"></div>
+    <div class="info">
 
-    <div class="herramientas">
-        <h2>Herramientas</h2>
+        <h2>Hola mi nombre es</h2>
+        <h1>CRISTOPHER BERNAL</h1>
+        <h3>y soy un qa tester</h3>
 
-        <p>
-        <img src="img/devops.png" width="40">
-        AZURE DEVOPS
+        <p class="descripcion">
+        Apasionado por la calidad del software y la mejora continua.
         </p>
 
-        <p>
-        <img src="img/word.jpg" width="40">
-        Microsoft Word
-        </p>
+        <div class="secciones">
+
+            <div class="proyectos">
+                <h2>Proyectos de Testing:</h2>
+
+                <p>
+                <img src="img/github.png">
+                Periodico Digital Comunitario
+                </p>
+
+                <p>
+                <img src="img/github.png">
+                Donamiga
+                </p>
+
+            </div>
+
+            <div class="linea"></div>
+
+            <div class="herramientas">
+
+                <h2>Herramientas</h2>
+
+                <p>
+                <img src="img/devops.png">
+                AZURE DEVOPS
+                </p>
+
+                <p>
+                <img src="img/word.jpg">
+                Microsoft Word
+                </p>
+
+            </div>
+
+        </div>
+
     </div>
 
 </div>
+
+<footer>
+
+<div class="footer-contenido">
+
+<div>
+<h3>Sobre mi</h3>
+</div>
+
+<div>
+<h3>Redes sociales</h3>
+<p>LinkedIn | GitHub | Email</p>
+</div>
+
+</div>
+
+<p class="copy">© 2026 Cristopher Bernal. Todos los derechos reservados.</p>
+
+</footer>
+
+</body>
+</html>
